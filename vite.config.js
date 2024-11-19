2544f5576f7958215ce1a794fe5e658ee28925c0
ganapati3 <124158856+ganapati3@users.noreply.github.com>
2024-11-19 16:19:47 +0530

    update: config updated for build version

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/user-management-dashboard/' : '/'
+  base: process.env.NODE_ENV === 'production' ? '/user-management-dashboard/' : '/',
+  build: {
+    outDir: 'dist', 
+  },
 })
